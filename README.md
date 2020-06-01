f57d59a928878938693a20c17fbcdf8442c884eb
Addis Semagn <asemagn@gmail.com>
2020-06-01 15:50:04 -0400

    Update README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Zoomer
+a fun lil bot for zoom.
+
+### Setup
+Clone repo and open directory
+```
+$  git clone git@github.com:addissemagn/zoomer.git && cd zoomer
+```
+
+You need NodeJs and npm installed. 
+
+Install web server:
+```
+$ sudo npm i -g local-web-server
+```
+
+Run server:
+```
+$ ws
+```
+
+If error that node version is not up to date, update node and try again:
+```
+$ sudo npm install -g n
+$ sudo n stable
+$ ws
+```
+
+Check it out at http://localhost:8000 :)
--- js/app.js
+++ js/app.js
@@ -1,12 +1,50 @@
+
 $(document).ready(function() {
-  const zoombot = new Artyom();
+  const zoomer = new Artyom();
   
   var on = false;
 
   $('#btnOnOff').click(function() {
     var speech = prompt("whatcha wanna say");
     if (speech != null) {
-      zoombot.say(speech)
+      zoomer.say(speech)
     }
+
+    startContinuousArtyom();
+
+  })
+
+
+  zoomer.addCommands({
+    indexes: ["Hello", "Hey", "How's it going?"],
+    action: function() {
+      zoomer.say("What's up gamers?");
+    }
+  });
+
+  zoomer.on(['Good morning','Good afternoon']).then(function(i){
+    console.log("listening");
   })
  })
+
+
+const artyom = new Artyom();
+
+// This function activates artyom and will listen all that you say forever (requires https conection, otherwise a dialog will request if you allow the use of the microphone)
+function startContinuousArtyom(){
+  artyom.fatality();// use this to stop any of
+
+  setTimeout(function(){// if you use artyom.fatality , wait 250 ms to initialize again.
+    artyom.initialize({
+      lang:"en-GB",// A lot of languages are supported. Read the docs !
+      continuous:true,// Artyom will listen forever
+      listen:true, // Start recognizing
+      debug:true, // Show everything in the console
+      speed:1 // talk normally
+    }).then(function(){
+      console.log("Ready to work !");
+              
+    });
+      },250);
+  
+}
